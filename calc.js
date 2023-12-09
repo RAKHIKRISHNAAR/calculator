9f5dbf8cc0696f75aee91ef92cced52c8362183c
unknown <rakhykrishnaa5ar@gmail.com>
2023-12-09 20:05:39 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,13 @@
+function displayData(e){
+    result.value+=e
+}
+function out(){
+    result.value=eval(result.value)
+}
+function Allclear(){
+    result.value=""
+}
+function backSpace(){
+    result.value=result.value.slice(0,-1)
+}
+
